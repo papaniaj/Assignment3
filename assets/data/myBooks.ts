cee165e198b30325c34e6e74ef16b9cd18f6c030
papaniaj <52494297+papaniaj@users.noreply.github.com>
2019-07-23 19:45:51 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,32 @@
+import { bookinfo } from '../../app/bookinfo';
+
+export const MYBOOKS : bookinfo[] =
+[
+    {   name : 'Halo: Glasslands',
+        author : 'Karen Travis',
+        genre : 'Action, Military Science Fiction',
+        year : '2011',
+        picture : 'assets/images/Halo_Glasslands.png'
+    },
+    {   
+        name : 'Halo: The Fall of Reach',
+        author : 'Eric Nylund',
+        genre : 'Action, Military Science Fiction',
+        year : '2001',
+        picture : 'assets/images/Halo_The_Fall_of_Reach.jpg'
+    },
+    {   
+        name : 'Halo: The Flood',
+        author : 'William C. Dietz',
+        genre : 'Action, Military Science Fiction',
+        year : '2003',
+        picture : 'assets/images/Halo_The_Flood.jpg'
+    },
+    {   
+        name : 'Halo: Silentium',
+        author : 'Greg Bear',
+        genre : 'Action, Military Science Fiction',
+        year : '2013',
+        picture : 'assets/images/Halo_Silentium.jpg'
+    }
+]
